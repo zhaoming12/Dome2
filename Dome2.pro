9616ab11e66af8434c58ffb350b1309ce99a8758
jasingou <oujianxin@techwinsz.com>
2019-05-25 09:46:31 +0800

    第一次修改

---
+++ Dome2.pro
@@ -0,0 +1,23 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2019-05-16T19:48:39
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Dome2
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+
+RESOURCES += \
+    image.qrc
---
+++ mainwindow.cpp
@@ -0,0 +1,38 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QPixmap>
+#include <QIcon>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //把背景设为黑色
+    ui->label->clear();
+    QPalette palette;
+    palette.setColor(QPalette::Background, QColor(0, 0, 0));
+    ui->label->setAutoFillBackground(true);  //一定要这句，否则不行,要绘制的图像区域
+    ui->label->setPalette(palette);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+//显示第一张图片
+void MainWindow::on_First_clicked()
+{
+    QPixmap *pixmap = new QPixmap(":/images/picture_1.png");
+    pixmap->scaled(ui->label->size(), Qt::KeepAspectRatio);
+    ui->label->setScaledContents(true); //自适应窗口的大小
+    ui->label->setPixmap(*pixmap);
+}
+//显示第二张图片
+void MainWindow::on_Second_clicked()
+{
+    QPixmap *pixmap = new QPixmap(":/images/picture_2.png");
+    pixmap->scaled(ui->label->size(), Qt::KeepAspectRatio);
+    ui->label->setScaledContents(true);
+    ui->label->setPixmap(*pixmap);
+}
